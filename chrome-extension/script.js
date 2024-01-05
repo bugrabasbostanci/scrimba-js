0d77cd7d0ae7ce95e8d0951ad9cd7a8b44559a40
bb0143sbw <bb0143sbw+github@gmail.com>
2024-01-05 23:52:35 +0300

    scrimba js day 5 completed

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,32 +1,68 @@
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-let myLeads = [];
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs);
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+        <li>
+        <a href="${leads[i]}" target="_blank">${leads[i]}</a>
+        </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = " ";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
 
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-        <li>
-        <a href="#${myLeads[i]}" target="_blank">${myLeads[i]}</a>
-        </li>
-    `;
+// challenge
+const imgs = ["images/hip1.jpg", "images/hip2.jpg", "images/hip3.jpg"];
+
+const containerEl = document.getElementById("container");
+
+function renderImages() {
+  for (let i = 0; i < imgs.length; i++) {
+    containerEl.innerHTML += `<img src="${imgs[i]}" class="team-img">`;
   }
 
-  ulEl.innerHTML = listItems;
+  // better performence
+  // function renderImages() {
+  //   let imgsDOM = "";
+  //   for (let i = 0; i < imgs.length; i++) {
+  //     imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`;
+  //   }
+  //   containerEl.innerHTML = imgsDOM;
+  // }
 }
+
+renderImages();
